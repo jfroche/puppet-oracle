e42b79b59305f32bd6c1bc4ed2d9ca24ddfff4df
Julien Pivotto <roidelapluie@inuits.eu>
2012-04-10 15:17:07 +0200

    Module oracle: add limitx

--- manifests/kern.pp
+++ manifests/kern.pp
@@ -7,4 +7,27 @@ class oracle::kern {
     'fs.file-max': value                  => '747266';
   }
 
+  limits::conf {
+    'oracle-nproc-soft':
+      domain => 'oracle',
+      type   => 'soft',
+      item   => 'nproc',
+      value  => 2047;
+    'oracle-nproc-hard':
+      domain => 'oracle',
+      type   => 'hard',
+      item   => 'nproc',
+      value  => 16384;
+    'oracle-nofile-soft':
+      domain => 'oracle',
+      type   => 'soft',
+      item   => 'nofile',
+      value  => 1024;
+    'oracle-nofile-hard':
+      domain => 'oracle',
+      type   => 'hard',
+      item   => 'nofile',
+      value  => 65536;
+  }
+
 }
