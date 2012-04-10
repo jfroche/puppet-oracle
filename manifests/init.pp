8764e1a7ffb331c04db0a5fa128e95c50b5491ef
Julien Pivotto <roidelapluie@inuits.eu>
2012-04-10 11:13:29 +0200

    Module oracle: I hate oracle chapter two

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,11 @@ class oracle {
   realize(User['oracle'], Group['oracle', 'dba'])
   realize(Yumrepo['oracle'])
 
+  package {
+    'glibc.i686':
+      ensure  => installed,
+      require => Class['yum::client']
+  }
   package{
     'oracledb':
       ensure  => installed,
