d109fa39007407cacce62ea207d1095be0bf929c
Julien Pivotto <roidelapluie@inuits.eu>
2012-04-10 12:38:18 +0200

    Module oracle: create database

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,5 +13,22 @@ class oracle {
       ensure  => installed,
       require => Yumrepo['oracle'],
   }
+  file {
+    '/opt/standard.rsp':
+      ensure  => present,
+      content => template('oracle/standard.rsp'),
+      require => Package['oracledb'],
+  }
+  exec {
+    '/opt/oracle/database/runInstaller -silent -responseFile /opt/standard.rsp':
+      user    => 'oracle',
+      require => [
+        File['/opt/standard.rsp'],
+        User['oracle'],
+        Group['oracle', 'dba'],
+        Package['oracledb'],
+      ],
+      creates => '/u01/app/oracle/oracle/product/10.2.0/db_1/',
+  }
 
 }
