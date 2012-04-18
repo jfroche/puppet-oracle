11d9fd1247563ca6666bbf1c7f3e34779323566c
Jean-François Roche <jfroche@affinitic.be>
2012-04-18 16:27:48 +0200

    add oracle bash environement variable

--- manifests/createdb.pp
+++ manifests/createdb.pp
@@ -1,10 +1,19 @@
 define oracle::createdb (
+  $oracle_home,
   $oradata,
   $sys_password,
   $system_password,
   $charset = 'AL32UTF8',
   $national_charset = 'AL16UTF16'
   ){
+
+  file {
+    '/etc/profile.d/oracle.sh':
+      content => "export ORACLE_HOME=$oracle_home;export ORACLE_SID=$name",
+      owner   => 'root',
+      mode    => '0644'
+  }
+
 
   file {
     "/oracle/createdb-$name.sql":
