ec91d7fa82e035a1519892290bb7b12673b8661d
Jean-François Roche <jfroche@affinitic.be>
2012-04-19 12:25:49 +0200

    add env variable install trick - we <3 oracle

--- manifests/createdb.pp
+++ manifests/createdb.pp
@@ -9,7 +9,7 @@ define oracle::createdb (
 
   file {
     '/etc/profile.d/oracle.sh':
-      content => "export ORACLE_HOME=$oracle_home;export ORACLE_SID=$name",
+      content => "export ORACLE_HOME=$oracle_home;export ORACLE_SID=$name;export export DISABLE_HUGETLBFS=1",
       owner   => 'root',
       mode    => '0644'
   }
