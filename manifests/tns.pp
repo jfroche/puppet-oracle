907fe5e35622c9e14a5731b0de28379af31226bb
Jean-François Roche <jfroche@affinitic.be>
2012-04-20 15:51:36 +0200

    add file if not present

--- manifests/tns.pp
+++ manifests/tns.pp
@@ -6,10 +6,19 @@ define oracle::tns (
 )
 {
 
+  file {
+    "$oracle_home/network/admin/tnsnames.ora":
+      ensure  => present,
+      owner   => 'oracle',
+      group   => 'oracle',
+      replace => false
+  }
+
   file_line {
     "tns_${name}_line":
-      path => "$oracle_home/network/admin/tnsnames.ora",
-      line => template('oracle/tnsname')
+      path    => "$oracle_home/network/admin/tnsnames.ora",
+      line    => template('oracle/tnsname'),
+      require => File["$oracle_home/network/admin/tnsnames.ora"]
   }
 
 }
