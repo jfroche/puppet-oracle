70a02bd58842a2e3cb4b6dcbae7c7a73bc7e8553
Jean-François Roche <jfroche@affinitic.be>
2012-08-21 12:28:45 +0200

    remove surrounding coma

--- templates/createfastmv.sql
+++ templates/createfastmv.sql
@@ -1,5 +1,5 @@
 create materialized view <%= name %> TABLESPACE USERS STORAGE ( INITIAL 320K NEXT 320k PCTINCREASE 0) USING INDEX TABLESPACE INDX STORAGE ( INITIAL 160k NEXT 160k PCTINCREASE 50) REFRESH FAST with primary key AS select * from <%= name %>@<%= dblink %>;
 GRANT SELECT on <%= name %> to <%= grant_to_user %>;
-exec DBMS_REFRESH.ADD(name => '"<%= refresh_group_schema %>"."<%= refresh_group_name %>"',list => '"<%= name.gsub(/(\w+)\.(\w+)/, '"\1"."\2"') %>"',lax => TRUE);
+exec DBMS_REFRESH.ADD(name => '"<%= refresh_group_schema %>"."<%= refresh_group_name %>"',list => '<%= name.gsub(/(\w+)\.(\w+)/, '"\1"."\2"') %>',lax => TRUE);
 quit
 
