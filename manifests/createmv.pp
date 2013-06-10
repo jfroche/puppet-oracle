f826ec20b8ab25d57165ffe42ff6ee5300073bae
Jean-François Roche <jfroche@affinitic.be>
2013-06-10 17:25:49 +0200

    enable mv without fast option - refs #5700

--- manifests/createmv.pp
+++ manifests/createmv.pp
@@ -2,14 +2,16 @@ define oracle::createmv (
   $dblink,
   $grant_to_users,
   $refresh_group_schema,
-  $refresh_group_name
+  $refresh_group_name,
+  $query = '',
+  $template = 'createfastmv.sql'
 )
 {
 
   file {
     "/oracle/mv/mv-${name}.sql":
       ensure  => present,
-      content => template('oracle/createfastmv.sql'),
+      content => template("oracle/${template}"),
       owner   => 'oracle',
       group   => 'oracle',
       require => File['/oracle/mv'],
---
+++ templates/createmv.sql
@@ -0,0 +1,7 @@
+create materialized view <%= name %> TABLESPACE USERS STORAGE ( INITIAL 320K NEXT 320k PCTINCREASE 0) USING INDEX TABLESPACE INDX STORAGE ( INITIAL 160k NEXT 160k PCTINCREASE 50) REFRESH FAST with primary key AS <%= query %>;
+<% grant_to_users.each do |user| %>
+GRANT SELECT on <%= name %> to <%= user %>;
+<% end %>
+exec DBMS_REFRESH.ADD(name => '"<%= refresh_group_schema %>"."<%= refresh_group_name %>"',list => '<%= name.gsub(/(\w+)\.(\w+)/, '"\1"."\2"') %>',lax => TRUE);
+quit
+
