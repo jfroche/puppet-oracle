13946b5c94a498fa2cd9235c667d7ac4d29dcf18
Martin Peeters <martin.peeters@affinitic.be>
2014-03-24 12:31:15 +0100

    Fix template syntax. refs #6598

--- templates/grant.sql
+++ templates/grant.sql
@@ -1,5 +1,5 @@
-<% users.each do |user| %>
-<% permissions.each do |permission| %>
-GRANT <%= permission %> on <%= object %> to <%= user %><%= if grant_option %>WITH GRANT OPTION<%= end %>;
-<% end %>
-<% end %>
+<%- users.each do |user| %>
+<%- permissions.each do |permission| %>
+GRANT <%= permission %> on <%= object %> to <%= user %><% if grant_option %>WITH GRANT OPTION<% end %>;
+<%- end %>
+<%- end %>
